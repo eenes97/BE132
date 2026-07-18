312f853d11659513c80d494c344520e2882074c8
eenes97 <eneskeskin0655@gmail.com>
2026-07-18 19:48:26 +0300

    hafta-04: hesap makinesi uygulaması geliştirildi

---
+++ Ders/Hafta 4 - Siliconmade Academy/BE132H04/HesapMakinesi.Console/Program.cs
@@ -0,0 +1,66 @@
+﻿string input = "";
+
+Console.Write("Sayi1: ");
+input = Console.ReadLine();
+double sayi1 = double.Parse(input);
+
+Console.Write("Sayi2: ");
+input = Console.ReadLine();
+double sayi2 = double.Parse(input);
+
+Console.Write("İslem: ");
+input = Console.ReadLine();
+string islem = input;
+
+double sonuc = 0;
+
+switch (islem)
+{
+
+    case "+":
+        sonuc = Topla(sayi1, sayi2);
+        break;
+    case "-":
+        sonuc = Cikar(sayi1, sayi2);
+        break;
+    case "*":
+        sonuc = Carp(sayi1, sayi2);
+        break;
+    case "/":
+        sonuc = Bol(sayi1, sayi2);
+        break;
+    default:
+        Console.WriteLine("Geçersiz işlem!");
+        break;
+}
+
+Console.WriteLine($"Sonuç: {sonuc}");
+
+double Topla(double sayi1, double sayi2)
+{
+    double sonuc = sayi1 + sayi2;
+    return sonuc;
+}
+
+double Cikar(double sayi1, double sayi2)
+{
+    double sonuc = sayi1 - sayi2;
+    return sonuc;
+}
+
+double Carp(double sayi1, double sayi2)
+{
+    double sonuc = sayi1 * sayi2;
+    return sonuc;
+}
+
+double Bol(double sayi1, double sayi2)
+{
+    if(sayi2 == 0)
+    {
+        Console.WriteLine("Sıfıra bölme hatası!");
+        return 0;
+    }
+    double sonuc = sayi1 / sayi2;
+    return sonuc;
+}
